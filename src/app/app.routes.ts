8a925c064442eafeb2a619d1dbe8176774dee9ad
Mann <mann.patel42@nmims.edu.in>
2024-03-03 03:40:01 +0530

    home page and login component added, favicon updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PosLoginComponent } from './pages/pos/pos-login/pos-login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '', component: HomeComponent
+    },
+    {
+        path: 'pos/login', component: PosLoginComponent
+    }
+];
