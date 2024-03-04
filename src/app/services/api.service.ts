36d1181d3e5ce52436126ff50bb5da9c58d53222
Mann <mann.patel42@nmims.edu.in>
2024-03-04 23:29:35 +0530

    form builder typescript setup

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  httpHeaderOptions:any;
+  userData:any;
+  header:any;
+  constructor(
+    public http: HttpClient
+  ) { 
+    this.setToken();
+  }
 
-  constructor() { }
+  setToken(){
+    let token: any;
+    token = localStorage.getItem('userLogin');
+    token = JSON.parse(token)
+    if(token){
+      this.header = {
+        headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+      }
+    }
+  }
+
+  loginAdmin(data:any){
+    return this.http.post(`${environment.apiUrl}/adminUser/login`, data)
+  }
 }
