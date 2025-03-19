3b2c0577bbad8bd72cf4ec32e8f0b2e78583bda4
Mann <mannp3005@gmail.com>
2025-03-19 15:50:42 +0530

    update url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // apiUrl: "http://localhost:8000/api/v1"
-   apiUrl: "https://chinkisaathi-backend.vercel.app/api"
+   apiUrl: "https://chinkisaathi-backend-git-vercel-test-mannpatel117s-projects.vercel.app/api"
     // apiUrl: "https://chinkisaathi-backend-2qt4ft99q-mannpatel117s-projects.vercel.app/api/v1"
     // apiUrl: "http://3.109.210.222:8000/api/v1"
 };
